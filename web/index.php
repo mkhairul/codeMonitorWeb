4cdd134170ad1ee5e8eea0c27d28e6a2fa683bac
mkhairul <mkhairul@gmail.com>
2015-03-16 02:51:47 +0800

    added config and includes parse stuffs

--- web/index.php
+++ web/index.php
@@ -1,6 +1,15 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+use Parse\ParseClient;
+use Parse\ParseObject;
+use Parse\ParseException;
+use Parse\ParseQuery;
+
+$config = require_once(__DIR__.'/../config/config.php');
+ParseClient::initialize($config['parse']['app_id'], 
+                        $config['parse']['rest_key'], 
+                        $config['parse']['master_key']);
 
 $app = new Silex\Application();
 $app['debug'] = true;
